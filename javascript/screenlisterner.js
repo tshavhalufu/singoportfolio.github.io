14b8f75e0905aae94977dfcf654ca4b679697197
tshavhalufu <70763506+tshavhalufu@users.noreply.github.com>
2024-01-05 16:15:16 +0200

    ui update

--- javascript/screenlisterner.js
+++ javascript/screenlisterner.js
@@ -1,18 +1,31 @@
-let sections = document.querySelectorAll('section');
-let navbarLinks = document.querySelectorAll('.navbarDiv .navbar .linkList .linkListTitle li a');
+let sections = document.querySelectorAll("section");
+let navbarLinks = document.querySelectorAll(
+  ".navbarDiv .navbar .linkList .linkListTitle li a"
+);
 
-window.onscroll = () =>{
-    sections.forEach( (section) =>{
-        let top = window.scrollY;
-        let offset = section.offsetTop - 100;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
+window.onscroll = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
 
-        if(top >= offset && top < offset + height){
-            navbarLinks.forEach((link) =>{
-                link.classList.remove('active');
-                document.querySelector('.navbarDiv .navbar .linkList .linkListTitle li a#'+id+'Link').classList.add('active')
-            });
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      navbarLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(
+            ".navbarDiv .navbar .linkList .linkListTitle li a#" + id + "Link"
+          )
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+function toggleMenu() {
+  const selectedMenu = document.querySelector(".menu__list");
+  const handburgerIcon = document.querySelector(".handburger__icon");
+  selectedMenu.classList.toggle("open_menu");
+  handburgerIcon.classList.toggle("open_menu");
 }
