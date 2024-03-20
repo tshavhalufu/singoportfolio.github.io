9fbc8d039e3defd9c63017bb4c9bc03e046b859a
tshavhalufu <70763506+tshavhalufu@users.noreply.github.com>
2024-03-20 08:42:56 +0200

    semantic html fix 1

--- javascript/screenlisterner.js
+++ javascript/screenlisterner.js
@@ -1,31 +1,24 @@
-let sections = document.querySelectorAll("section");
-let navbarLinks = document.querySelectorAll(
-  ".navbarDiv .navbar .linkList .linkListTitle li a"
-);
+document.addEventListener("DOMContentLoaded", () => {
+  let sections = document.querySelectorAll("section");
+  let navbarLinks = document.querySelectorAll(
+    ".navbar nav .linkListTitle li a"
+  );
 
-window.onscroll = () => {
-  sections.forEach((section) => {
-    let top = window.scrollY;
-    let offset = section.offsetTop - 100;
-    let height = section.offsetHeight;
-    let id = section.getAttribute("id");
+  window.onscroll = () => {
+    sections.forEach((section) => {
+      let top = window.scrollY;
+      let offset = section.offsetTop - 100;
+      let height = section.offsetHeight;
+      let id = section.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navbarLinks.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector(
-            ".navbarDiv .navbar .linkList .linkListTitle li a#" + id + "Link"
-          )
-          .classList.add("active");
-      });
-    }
-  });
-};
-
-function toggleMenu() {
-  const selectedMenu = document.querySelector(".menu__list");
-  const handburgerIcon = document.querySelector(".handburger__icon");
-  selectedMenu.classList.toggle("open_menu");
-  handburgerIcon.classList.toggle("open_menu");
-}
+      if (top >= offset && top < offset + height) {
+        navbarLinks.forEach((link) => {
+          link.classList.remove("active");
+          document
+            .querySelector(".navbar nav .linkListTitle li a#" + id + "Link")
+            .classList.add("active");
+        });
+      }
+    });
+  };
+});
